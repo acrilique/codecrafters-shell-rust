f280edffef8c65915343524c840b7b9852eb9970
Lluc Simó <lluc.simo@protonmail.com>
2026-01-05 20:35:07 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,10 @@
 use std::io::{self, Write};
 
 fn main() {
-    let command = &mut String::new();
-
     print!("$ ");
     io::stdout().flush().unwrap();
 
+    let command = &mut String::new();
     io::stdin().read_line(command).unwrap();
-
     print!("{}: command not found", command.trim());
-    io::stdout().flush().unwrap();
 }
