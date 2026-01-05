72761db4ff28acebc5a5bdec1ba13f0040544c8d
Lluc Simó <lluc.simo@protonmail.com>
2026-01-05 22:32:23 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,33 +9,35 @@ fn main() {
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer).unwrap();
         let command = buffer.trim();
-
-        if command == "exit" {
-            break;
-        }
-
         let tokens: Vec<&str> = command.split_whitespace().collect();
 
         if tokens.is_empty() {
             continue;
         }
 
-        if tokens[0] == "echo" {
-            println!("{}", tokens[1..].join(" "));
-        } else if tokens[0] == "type" {
-            if tokens.len() < 2 {
-                continue;
+        match tokens[0] {
+            "exit" => {
+                break;
             }
-            match tokens[1] {
-                "echo" | "exit" | "type" => {
-                    println!("{} is a shell builtin", tokens[1]);
+            "echo" => {
+                println!("{}", tokens[1..].join(" "));
+            }
+            "type" => {
+                if tokens.len() < 2 {
+                    continue;
                 }
-                _ => {
-                    println!("{}: not found", tokens[1]);
+                match tokens[1] {
+                    "exit" | "echo" | "type" => {
+                        println!("{} is a shell builtin", tokens[1]);
+                    }
+                    _ => {
+                        println!("{}: not found", tokens[1]);
+                    }
                 }
             }
-        } else {
-            println!("{}: command not found", tokens[0]);
+            _ => {
+                println!("{}: command not found", tokens[0]);
+            }
         }
     }
 }
