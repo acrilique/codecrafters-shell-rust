163228f3e3c8b8c2a1b810a652d796e4d5bed0a9
Lluc Simó <lluc.simo@protonmail.com>
2026-01-05 20:31:50 +0100

    handle dle invalid commands

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@
 use std::io::{self, Write};
 
 fn main() {
+    let command = &mut String::new();
+
     print!("$ ");
+    io::stdout().flush().unwrap();
+
+    io::stdin().read_line(command).unwrap();
+
+    print!("{}: command not found", command.trim());
     io::stdout().flush().unwrap();
 }
